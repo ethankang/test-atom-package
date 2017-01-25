411a1926b677c0d7e6256f6d87d935203d93b1d7
Ricardo N Feliciano <FelicianoTech@gmail.com>
2017-01-24 22:26:49 -0500

    Initial commit, test current CI process.

---
+++ spec/commands-spec.js
@@ -0,0 +1,31 @@
+/* @flow */
+
+import Path from 'path'
+import { it } from 'jasmine-fix'
+import Commands from '../lib/commands'
+
+let commands
+
+describe('Linter Commands', function() {
+  beforeEach(function() {
+    if (commands) {
+      commands.dispose()
+    }
+    commands = new Commands()
+  })
+  it('properly notifies its listeners of command execution', async function() {
+    let lintCalled = 0
+    let toggleCalled = 0
+
+    commands.onShouldLint(() => ++lintCalled)
+    commands.onShouldToggleActiveEditor(() => ++toggleCalled)
+
+    await atom.workspace.open(Path.join(__dirname, 'fixtures', 'file.txt'))
+    const textEditor = atom.views.getView(atom.workspace.getActiveTextEditor())
+    atom.commands.dispatch(textEditor, 'linter:lint')
+    atom.commands.dispatch(textEditor, 'linter:toggle-active-editor')
+
+    expect(lintCalled).toBe(1)
+    expect(toggleCalled).toBe(1)
+  })
+})
